4ad3ea8c392dc64b85dbb92495d0ebfc7ab0c511
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-07-19 22:29:07 -0400

    add event listeners and their callbacks

--- src/components/ComicForm.js
+++ src/components/ComicForm.js
@@ -1,21 +1,21 @@
-function ComicForm() {
+function ComicForm({ handleSubmit, handleChange }) {
   return (
 
-    <form className="comic-form">
+    <form className="comic-form" onSubmit={handleSubmit} >
 
       <h2>Add A New Issue</h2>
 
       <label htmlFor="image_url">Image URL: </label>
-      <input name="image_url" />
+      <input name="image_url" onChange={handleChange}/>
 
       <label htmlFor="title">Title: </label>
-      <input name="title" />
+      <input name="title" onChange={handleChange} />
 
       <label htmlFor="issue">Issue Number: </label>
-      <input name="issue" type="number" />
+      <input name="issue" type="number" onChange={handleChange} />
 
       <label htmlFor="description">Description: </label>
-      <input name="description" />
+      <input name="description" onChange={handleChange} />
 
       <input type="submit" value="Add Issue" />
 
