74ab6b756e9213d96f14a95dceb2af3dea6446d5
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-07-19 22:26:23 -0400

    define comic parameters

--- src/components/Comic.js
+++ src/components/Comic.js
@@ -1,16 +1,22 @@
-function Comic() {
+import { useState } from "react"
+
+function Comic({ comic, URL, removeComic }) {
+  const [image, setImage] = useState(true)
+
+  function handleDelete () {
+    fetch(`${URL}/${comic.id}`, {method: "DELETE"})
+    removeComic(comic)
+  }
 
   return (
-    <div className="comic-item">
+    <div className="comic-item" onClick={()=>setImage(!image)}>
 
-      {/* The image should render if the details aren't displayed */}
-      <img src={"#"} alt={"Comic Issue Image"} />
-
-      {/* The details should render if the image isn't displayed */}
-      <h3>{"Title"}</h3>
-      <h4>{"Issue No."}</h4>
-      <button>Remove</button>
-
+      {image ? <img src={comic.image_url} alt={comic.issue} /> :
+        <>
+          <h3>{comic.title}</h3>
+          <h4>{comic.issue}</h4>
+          <button onClick={handleDelete}>Remove</button>
+        </>}
     </div>
   )
 
