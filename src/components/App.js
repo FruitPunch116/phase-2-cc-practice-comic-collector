2ea7b3f66f0bbba29da03dbc13eeb695f2ec058c
Angel Figueroa <gabrielfigue4120@gmail.com>
2023-07-19 22:22:32 -0400

    functions, create 'POST', 'Get', 'DELETE' request, props

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,53 @@
 import ComicsContainer from "./ComicsContainer"
 import ComicForm from "./ComicForm"
+import { useEffect, useState } from "react";
+
+const PORT = 8004
+const URL = `http://localhost:${PORT}/comics`
 
 function App() {
+  
+  const [comics, setComics] = useState([]);
+  useEffect( () => {
+    fetch(URL)
+    .then(resp => resp.json())
+    .then(data => setComics(data))
+    // .then(data => console.log(data))
+  },[]);
+  
+  const [newComic,  setNewComic] = useState({})
+
+  function handleSubmit (e) {
+    e.preventDefault()
+    //post to backend (fetch)
+    fetch(URL, {
+      method: "POST",
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newComic)
+    })
+    //update frontent (usestate)
+    setComics([...comics,newComic])
+
+  };
+
+  function handleChange (e) {
+    // console.log(e.target.name)
+    setNewComic({
+      ...newComic,
+       [e.target.name]: e.target.value
+      
+    })
+
+  };
+
+  function removeComic (comicToDelete) {
+    const filteredArray = comics.filter((comic)=> comic.id !== comicToDelete.id)
+    setComics(filteredArray)
+  }
+
   return (
     <div className="App">
 
@@ -10,11 +56,11 @@ function App() {
       <div className="grid with-sidebar">
 
         <div className="flex-container">
-          <ComicsContainer />
+          <ComicsContainer comics={comics} URL={URL} setComics={setComics} removeComic={removeComic} />
         </div>
 
         <div className="sidebar">
-          <ComicForm />
+          <ComicForm handleChange={handleChange} handleSubmit={handleSubmit} />
         </div>
 
       </div>
